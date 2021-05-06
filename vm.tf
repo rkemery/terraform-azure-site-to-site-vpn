3acb972b1eb94626b33cc816c67c2f10b1293bf2
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-05-06 15:05:01 -0400

    enforce to true

--- vm.tf
+++ vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "vm" {
   resource_group_name                            = azurerm_resource_group.example.name
   virtual_network_name                           = azurerm_virtual_network.example.name
   address_prefixes                               = var.vm_address_prefixes
-  enforce_private_link_endpoint_network_policies = false
+  enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_network_interface" "main" {
