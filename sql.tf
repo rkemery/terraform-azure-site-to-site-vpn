5e0224a06215bd0647f6469afe629553f027105b
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-05-06 14:56:08 -0400

    re-create subnets, sql, and vm

--- sql.tf
+++ sql.tf
@@ -1,54 +1,54 @@
-// resource "azurerm_sql_server" "example" {
-//   name                         = var.sql_server_name
-//   resource_group_name          = azurerm_resource_group.example.name
-//   location                     = var.location
-//   version                      = var.sql_server_version
-//   administrator_login          = var.sql_server_admin_login
-//   administrator_login_password = var.sql_server_admin_password
+resource "azurerm_sql_server" "example" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = var.location
+  version                      = var.sql_server_version
+  administrator_login          = var.sql_server_admin_login
+  administrator_login_password = var.sql_server_admin_password
 
-//   tags = {
-//     environment = var.sql_server_tags
-//   }
-// }
+  tags = {
+    environment = var.sql_server_tags
+  }
+}
 
-// resource "azurerm_private_endpoint" "endpoint" {
-//   name                         = var.private_endpoint_name
-//   resource_group_name          = azurerm_resource_group.example.name
-//   location                     = azurerm_resource_group.example.location
-//   subnet_id                    = azurerm_subnet.vm.id
+resource "azurerm_private_endpoint" "endpoint" {
+  name                         = var.private_endpoint_name
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  subnet_id                    = azurerm_subnet.vm.id
 
-//   private_service_connection {
-//     is_manual_connection           = false
-//     name                           = var.private_endpoint_connection_name
-//     private_connection_resource_id = azurerm_sql_server.example.id
-//     subresource_names              = ["sqlServer"]
-//   }
-// }
+  private_service_connection {
+    is_manual_connection           = false
+    name                           = var.private_endpoint_connection_name
+    private_connection_resource_id = azurerm_sql_server.example.id
+    subresource_names              = ["sqlServer"]
+  }
+}
 
-// resource "azurerm_storage_account" "example" {
-//   name                     = var.sql_server_storage_account_name
-//   resource_group_name      = azurerm_resource_group.example.name
-//   location                 = azurerm_resource_group.example.location
-//   account_tier             = var.sql_server_storage_account_tier
-//   account_replication_type = var.sql_server_storage_account_type
-// }
+resource "azurerm_storage_account" "example" {
+  name                     = var.sql_server_storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.sql_server_storage_account_tier
+  account_replication_type = var.sql_server_storage_account_type
+}
 
-// resource "azurerm_sql_database" "example" {
-//   name                = var.sql_db_name
-//   resource_group_name = azurerm_resource_group.example.name
-//   location            = var.location
-//   server_name         = azurerm_sql_server.example.name
+resource "azurerm_sql_database" "example" {
+  name                = var.sql_db_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  server_name         = azurerm_sql_server.example.name
 
-//   extended_auditing_policy {
-//     storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
-//     storage_account_access_key              = azurerm_storage_account.example.primary_access_key
-//     storage_account_access_key_is_secondary = true
-//     retention_in_days                       = 6
-//   }
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
 
 
 
-//   tags = {
-//     environment = var.sql_db_tags
-//   }
-// }
+  tags = {
+    environment = var.sql_db_tags
+  }
+}
--- vm.tf
+++ vm.tf
@@ -1,43 +1,43 @@
-// resource "azurerm_subnet" "vm" {
-//   name                                           = var.vm_subnet_name
-//   resource_group_name                            = azurerm_resource_group.example.name
-//   virtual_network_name                           = azurerm_virtual_network.example.name
-//   address_prefixes                               = var.vm_address_prefixes
-//   enforce_private_link_endpoint_network_policies = false
-// }
+resource "azurerm_subnet" "vm" {
+  name                                           = var.vm_subnet_name
+  resource_group_name                            = azurerm_resource_group.example.name
+  virtual_network_name                           = azurerm_virtual_network.example.name
+  address_prefixes                               = var.vm_address_prefixes
+  enforce_private_link_endpoint_network_policies = false
+}
 
-// resource "azurerm_network_interface" "main" {
-//   name                = var.nic_name
-//   location            = azurerm_resource_group.example.location
-//   resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "main" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-//   ip_configuration {
-//     name                          = var.nic_ip_config_name
-//     subnet_id                     = azurerm_subnet.vm.id
-//     private_ip_address_allocation = var.nic_private_ip_addr_allocation
-//   }
-// }
+  ip_configuration {
+    name                          = var.nic_ip_config_name
+    subnet_id                     = azurerm_subnet.vm.id
+    private_ip_address_allocation = var.nic_private_ip_addr_allocation
+  }
+}
 
-// resource "azurerm_windows_virtual_machine" "example" {
-//   name                = var.vm_name
-//   resource_group_name = azurerm_resource_group.example.name
-//   location            = azurerm_resource_group.example.location
-//   size                = var.vm_size
-//   admin_username      = var.vm_admin_username
-//   admin_password      = var.vm_admin_password
-//   network_interface_ids = [
-//     azurerm_network_interface.main.id,
-//   ]
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.vm_size
+  admin_username      = var.vm_admin_username
+  admin_password      = var.vm_admin_password
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
 
-//   os_disk {
-//     caching              = "ReadWrite"
-//     storage_account_type = "Standard_LRS"
-//   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-//   source_image_reference {
-//     publisher = var.image_publisher
-//     offer     = var.image_offer
-//     sku       = var.image_sku
-//     version   = var.image_version
-//   }
-// }
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+}
