6bcc7ae73821cd2df9e02d4a63342d0baf1fb3c4
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-05-06 00:17:49 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "vpn-gateway" {
+  source  = "kumarvna/vpn-gateway/azurerm"
+  version = "1.0.0"
+
+  # Resource Group, location, VNet and Subnet details
+  # IPSec Site-to-Site connection configuration requirements
+  resource_group_name  = var.rg_name
+  virtual_network_name = var.vnet_name
+  vpn_gateway_name     = var.vpn_gw_name
+  gateway_type         = var.vpn_gw_type
+
+  # local network gateway connection
+  local_networks = [
+    {
+      local_gw_name         = var.local_gw_name
+      local_gateway_address = var.local_gw_addr
+      local_address_space   = var.local_addr_space
+      shared_key            = var.local_shared_key
+    },
+  ]
+
+  # Adding TAG's to your Azure resources (Required)
+  tags = {
+    ProjectName  = var.project_name
+    Env          = var.env
+    Owner        = var.owner
+    BusinessUnit = var.bu
+    ServiceClass = var.service_class
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "rg_name" {
+}
+
+variable "vnet_name" {
+}
+
+variable "vpn_gw_name" {
+}
+
+variable "vpn_gw_type" {
+}
+
+variable "local_gw_name" {
+}
+
+variable "local_gw_addr" {
+}
+
+variable "local_addr_space" {
+  type = list
+}
+
+variable "local_shared_key" {
+}
+
+variable "project_name" {
+}
+
+variable "env" {
+}
+
+variable "owner" {
+}
+
+variable "bu" {
+}
+
+variable "service_class" {
+}
+
+variable "subscripton_id" {
+}
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "tenant_id" {
+}
